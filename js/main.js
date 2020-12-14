6e1e8849021238926a8d0b5ef886ca455939e26f
jose flores <jotajotadev@gmail.com>
2020-12-14 16:48:16 -0500

    order some of javascript

--- js/main.js
+++ js/main.js
@@ -1,38 +1,31 @@
 let mycard = document.querySelectorAll(".card");
 let menu = document.getElementById("menu");
 let buttonShowNav = document.getElementById("buttonForShowNav");
+
+// Toggle menu mobile
 buttonShowNav.addEventListener("click", function(){
-    let navExist = document.querySelector(".header__nav-show");
-    if(navExist) {
-        menu.setAttribute("class", "header__nav");
-        console.log(menu);
-    } else {
-        menu.setAttribute("class", "header__nav-show");
-        console.log(menu);
-    }
+    menu.classList.toggle("header__nav-show");
 });
 
+
+// Add class Card- Expand to Small Cards.
 for(let e of mycard){
     e.addEventListener("click", function(){
         let cardExpand = document.querySelector(".card-expand");
-        let viewportWidth = window.innerWidth;
-        if(viewportWidth <= 767){
+        if(window.innerWidth <= 767){
             if(cardExpand) {
                 console.log("no se vuelve a ejecutar")
             } else {
                 e.className = "card-expand";
-                let bc = e.parentElement;
-                bc.className = "bacground-card-active";
-                console.log(bc);
+                let backgrounCard = e.parentElement;
+                backgrounCard.className = "bacground-card-active";
                 let close = document.createElement("img");
                 close.id = "close-card";
                 close.setAttribute("src", "img/icon-close.png");
-                // let contentClose = document.createTextNode("X");
-                // close.appendChild(contentClose);
-                bc.insertBefore(close, e);
+                backgrounCard.insertBefore(close, e);
                 console.log(close);
                 close.addEventListener("click", function(){
-                    bc.className = "bacground-card";
+                    backgrounCard.className = "bacground-card";
                     e.className = "card";
                     close.style.display = "none";
                 });
@@ -42,5 +35,5 @@ for(let e of mycard){
             console.log("no se ejecutara");
         }
     });
-    // let close = document.getElementById("close-card")
 }
+
